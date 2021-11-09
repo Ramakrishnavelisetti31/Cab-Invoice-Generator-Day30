1ed8902659d0a99b8f47244abc2d537a85c3485d
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-11-09 23:40:03 +0530

    Added : welcome message

---
+++ src/main/java/com/bdlz/cabinvoice/CabInvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.bdlz.cabinvoice;
+
+public class CabInvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Cab Invoice Generator program");
+    }
+}
