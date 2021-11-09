e6e6c657f16167ef79a3e67e224830b3f5d5e61c
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-11-10 00:06:47 +0530

    Added : Calculating fare by taking distance and time

--- src/main/java/com/bdlz/cabinvoice/CabInvoiceGenerator.java
+++ src/main/java/com/bdlz/cabinvoice/CabInvoiceGenerator.java
@@ -1,7 +1,12 @@
 package com.bdlz.cabinvoice;
 
 public class CabInvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Cab Invoice Generator program");
+    private static final double COST_PER_KM = 10;
+    private static final double COST_PER_MIN = 1;
+    private static final double MIN_FARE = 5.0;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        return Math.max(totalFare, MIN_FARE);
     }
 }
